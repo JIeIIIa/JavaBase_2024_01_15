fdbfefe02f2f94c4e52a3161e35a1b4f67ccf45e
Oleksii Onishchenko <stealth@bigmir.net>
2024-02-15 20:18:47 +0200

    Lecture-10 - Overloaded constructors

--- com/gmail/onishchenko/lecture10/Shape.java
+++ com/gmail/onishchenko/lecture10/Shape.java
@@ -3,16 +3,34 @@ package com.gmail.onishchenko.lecture10;
 public class Shape {
     String name;
     String color;
+    int size;
 
     public Shape() {
-//        name = "Abstract shape";
-//        color = "black";
+        name = "Abstract shape";
+        color = "black";
+        size = 10;
     }
 
     //
-    public Shape(String shapeName) {
-        name = shapeName;
-        color = "black";
+    public Shape(String name) {
+        this();
+        this.name = name;
+    }
+
+    public Shape(String name, String color) {
+        this(name);
+        this.color = color;
+    }
+
+    public Shape(String name, String color, int size) {
+        this(name, color);
+        this.size = size;
+    }
+
+    public void greeting() {
+        String name = "Fake name";
+        System.out.println("Hello, I'm a " + name);
+        System.out.println("My actual name is " + this.name);
     }
 }
 
@@ -23,7 +41,8 @@ class ShapeRunner {
         shape.color = "black";
         System.out.println("Shape is created with name " + shape.name);
 
-        Shape anotherShape = new Shape("PrettyShape");
+        Shape anotherShape = new Shape("PrettyShape", "green", 42);
         System.out.println("Shape is created with name " + anotherShape.name);
+        anotherShape.greeting();
     }
 }
