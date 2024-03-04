4cb7a36063aa55a66236b85fa0709dd9f13b92ae
Oleksii Onishchenko <stealth@bigmir.net>
2024-03-04 20:27:03 +0200

    Lecture-14 - Junit (console running)

--- com/gmail/onishchenko/lecture13/CalculatorTest.java
+++ com/gmail/onishchenko/lecture13/CalculatorTest.java
@@ -1,5 +1,8 @@
 package com.gmail.onishchenko.lecture13;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
     public static void main(String[] args) {
         CalculatorTest.runAllTests();
@@ -8,25 +11,37 @@ public class CalculatorTest {
 
     public static void runAllTests() {
         System.out.println("  -= RUN ALL TESTS =-");
-        factorialTest();
-        factorialZeroTest();
+        CalculatorTest calculatorTest = new CalculatorTest();
+        calculatorTest.factorialTest();
+        calculatorTest.factorialZeroTest();
     }
 
-    public static void factorialTest() {
+    @Test
+    public void factorialTest() {
+        // given
         Calculator calculator = new Calculator();
         int value = 4;
+        int expected = 24;
+
+        // when
         long factorial = calculator.factorial(value);
 
+        // then
         System.out.println("Actual value: " + value + "! == " + factorial);
         System.out.println("Expected value: 4! == 24");
+        Assertions.assertEquals(expected, factorial);
     }
 
-    public static void factorialZeroTest() {
+    @Test
+    public void factorialZeroTest() {
         Calculator calculator = new Calculator();
         int value = 0;
+        int expected = 1;
+
         long factorial = calculator.factorial(value);
 
         System.out.println("Actual value: " + value + "! == " + factorial);
         System.out.println("Expected value: 0! == 1");
+        Assertions.assertEquals(expected, factorial);
     }
 }
