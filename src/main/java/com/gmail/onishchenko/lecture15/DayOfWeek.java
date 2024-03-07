b578c286fdaa64a307cde99a3654acc3620b340d
Oleksii Onishchenko <stealth@bigmir.net>
2024-03-07 21:16:09 +0200

    Lecture-15 - Enum: use static constants
    (cherry picked from commit a5ddd71fc2240507545861c13c09478f5158715d)

--- src/main/java/com/gmail/onishchenko/lecture15/DayOfWeek.java
+++ src/main/java/com/gmail/onishchenko/lecture15/DayOfWeek.java
@@ -1,19 +1,54 @@
 package com.gmail.onishchenko.lecture15;
 
+import java.util.Objects;
+
 public class DayOfWeek {
     public static void main(String[] args) {
         DayOfWeek dayOfWeek = new DayOfWeek();
-        String result = dayOfWeek.recommendation("QWERTY");
+        String result = dayOfWeek.recommendation(Day.MONDAY);
 
         System.out.printf("Recommendation is '%s'", result);
     }
 
-    public String recommendation(String day) {
-        if ("monday".equalsIgnoreCase(day)) {
+    public String recommendation(Day day) {
+        if (Day.MONDAY.equals(day)) {
             return "go to work!";
-        } else if ("Sunday".equalsIgnoreCase(day)) {
+        } else {
             return "You may sleep all day)";
         }
-        return "unknown day(((";
+
+    }
+}
+
+
+class Day {
+    public static final Day MONDAY = new Day("Monday");
+    public static final Day SUNDAY = new Day("Sunday");
+
+    private final String name;
+
+    private Day(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Day day = (Day) o;
+        return name != null && name.equalsIgnoreCase(day.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
