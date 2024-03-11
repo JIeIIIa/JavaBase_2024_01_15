4594983528b20b1afa6629d90c52227f76c1adbe
Oleksii Onishchenko <stealth@bigmir.net>
2024-03-11 20:19:23 +0200

    Lecture-16 - Nested static class

---
+++ src/main/java/com/gmail/onishchenko/lecture16/NestedClassesDemo.java
@@ -0,0 +1,9 @@
+package com.gmail.onishchenko.lecture16;
+
+public class NestedClassesDemo {
+    public static void main(String[] args) {
+        Library.Book book = new Library.Book("Pretty book");
+
+        System.out.println(book);
+    }
+}
